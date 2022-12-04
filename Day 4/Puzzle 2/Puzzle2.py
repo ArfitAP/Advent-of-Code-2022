bb990ddc08b3357c37689941b65e43569e18016a
AntonioPavlis <antonio.pavlis51@gmail.com>
2022-12-04 09:53:41 +0100

    Day 4

---
+++ Day 4/Puzzle 2/Puzzle2.py
@@ -0,0 +1,25 @@
+
+file = open('../input.txt', 'r')
+count = 0
+
+while True:
+
+    # Get next line from file
+    line = file.readline()
+
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+
+    firstElf, secondElf = line.split(",")
+
+    firstElfList = [i for i in range(int(firstElf.split("-")[0]), int(firstElf.split("-")[1]) + 1)]
+    secondElfList = [i for i in range(int(secondElf.split("-")[0]), int(secondElf.split("-")[1]) + 1)]
+
+    if not set(firstElfList).isdisjoint(set(secondElfList)):
+        count += 1
+
+print(count)
+
+file.close()
