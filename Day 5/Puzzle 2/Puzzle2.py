41f9828d53fa7d0fbd6b631a02ba03bd66ac2b21
AntonioPavlis <antonio.pavlis51@gmail.com>
2022-12-06 20:38:04 +0100

    Day 5-6

---
+++ Day 5/Puzzle 2/Puzzle2.py
@@ -0,0 +1,50 @@
+
+file = open('../input.txt', 'r')
+count = 0
+stackRead = False
+stack = []
+move = 0
+fromStack = 0
+toStack = 0
+
+while True:
+
+    # Get next line from file
+    line = file.readline()
+
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+
+    if len(line) == 1:
+        stackRead = True
+        continue
+
+    if len(stack) == 0:
+        for i in range(int(len(line) / 4)):
+            stack.append([])
+
+    if stackRead is False:
+        s = 0
+        for i in range(1, len(line) - 1, 4):
+            if line[i] != " ":
+                stack[s].insert(0, line[i])
+            s += 1
+    else:
+        move = int(line[4:].split("from")[0].strip())
+        fromStack = int(line.split("from")[1].split("to")[0].strip())
+        toStack = int(line.split("from")[1].split("to")[1].strip())
+
+        tmpIndex = len(stack[toStack - 1])
+        for i in range(move):
+            tmp = stack[fromStack - 1].pop()
+            stack[toStack - 1].insert(tmpIndex, tmp)
+
+res = ""
+for s in stack:
+    res += s[len(s) - 1]
+
+print(res)
+
+file.close()
---
+++ Day 6/Puzzle 2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Puzzle_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            int count = 0;
+            List<char> window = new();
+
+            StreamReader reader;
+            reader = new StreamReader(@"../../../../input.txt");
+            do
+            {
+                ch = (char)reader.Read();
+
+                count++;
+
+                if (window.Count < 14) window.Add(ch);
+                else
+                {
+                    window.RemoveAt(0);
+                    window.Add(ch);
+                }
+
+                if (window.Count >= 14 && checkAllDifferent(window)) break;
+
+
+            } while (!reader.EndOfStream);
+
+            reader.Close();
+            reader.Dispose();
+
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+
+        static bool checkAllDifferent(List<char> chars)
+        {
+            return chars.Distinct().Count() == chars.Count;
+        }
+    }
+}
