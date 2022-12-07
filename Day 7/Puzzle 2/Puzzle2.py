fc5eaaf828d42d346138a4e28d1c45c6894b7965
AntonioPavlis <antonio.pavlis51@gmail.com>
2022-12-07 19:47:26 +0100

    Day 7

---
+++ Day 7/Puzzle 2/Puzzle2.py
@@ -0,0 +1,122 @@
+
+class Node(object):
+    # Initializing to None
+    def __init__(self):
+        self.leafs = []
+        self.folder = True
+        self.size = 0
+        self.path = ["/"]
+
+
+def getNodeByPath(node, path):
+
+    if node.path == path:
+        return node
+    else:
+        newFolder = path[0:len(node.path) + 1]
+        for leaf in node.leafs:
+            if leaf.path == newFolder:
+                return getNodeByPath(leaf, path)
+
+        newleaf = Node()
+        newleaf.path = newFolder
+        node.leafs.append(newleaf)
+        return newleaf
+
+def getFolderSizes(node):
+
+    if not node.folder:
+        return node.size
+
+    sum = 0
+    for l in node.leafs:
+        size = getFolderSizes(l)
+        sum += size
+
+    pathstring = getpathfromstack(node.path)
+    if node.folder and (pathstring not in sizes.keys()):
+        sizes[pathstring] = sum
+
+    return sum
+
+def getpathfromstack(pathStack):
+    res = ""
+    for s in pathStack:
+        res += (s + "_")
+    return res
+
+
+file = open('../input.txt', 'r')
+
+tree = Node()
+
+currentNode = None
+currentFolder = ""
+pathStack = []
+
+sizes = {}
+
+while True:
+
+    # Get next line from file
+    line = file.readline()
+
+    # if line is empty
+    # end of file is reached
+    if not line:
+        break
+
+    if line[0] == "$":
+        if line.split(" ")[1] == "cd":
+            folderCmd = line.split(" ")[2].strip()
+
+            if folderCmd == "..":
+                currentFolder = pathStack.pop()
+            elif folderCmd == "/":
+                currentFolder = "/"
+                pathStack = ["/"]
+            else:
+                currentFolder = folderCmd
+                pathStack.append(currentFolder)
+
+        elif line.split(" ")[1].strip() == "ls":
+            currentNode = getNodeByPath(tree, pathStack)
+
+    else:
+        added = False
+        filePath = pathStack.copy()
+        filePath.append(line.split(" ")[1].strip())
+
+        for leaf in currentNode.leafs:
+            if leaf.path == filePath:
+                added = True
+
+        if not added:
+            newFolder = Node()
+            newFolder.path = filePath
+            if line.split(" ")[0] == "dir":
+                newFolder.folder = True
+            else:
+                newFolder.folder = False
+                newFolder.size = int(line.split(" ")[0])
+            currentNode.leafs.append(newFolder)
+
+
+getFolderSizes(tree)
+
+filteredDict = dict()
+requiredSpace = sizes.get("/_") - 40000000
+
+for (key, value) in sizes.items():
+    if value >= requiredSpace:
+        filteredDict[key] = value
+
+
+min = filteredDict.get("/_")
+for values in filteredDict.values():
+    if values < min:
+        min = values
+
+print(min)
+
+file.close()
